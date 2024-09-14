1ba0bc7db52eda2c0049c2dcd6f5438e53fd1488
Ricardo Zancan <ricardozancanc@gmail.com>
2024-09-14 18:55:49 -0300

    Initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+    <nav class="bg-black">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <h1 class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HolidaySearch</h1>
+        </a>
+        
+    </div>
+    </nav>
+</body>
+</html>
