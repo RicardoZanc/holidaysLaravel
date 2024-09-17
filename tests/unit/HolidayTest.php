09a91ce76d10629196a753ede0a03f229db0c6fd
Ricardo Zancan <ricardozancanc@gmail.com>
2024-09-16 21:13:06 -0300

    first unit test done

---
+++ tests/unit/HolidayTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace tests;
+
+use App\Http\Controllers\HolidayController;
+use PHPUnit\Framework\TestCase;
+
+class HolidayTest extends TestCase {
+
+    public function testFormatDate(){
+        $controller = new HolidayController();
+        $dateToConvert = "2024-02-01";
+        $convertedDate = $controller->formatDate($dateToConvert);
+        $this->assertEquals("01/02/2024", $convertedDate);
+    }
+    
+}
