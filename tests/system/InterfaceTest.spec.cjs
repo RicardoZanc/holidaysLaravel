5fa723118c71fa28fe6c711c0f6ca3cbbf19d180
Ricardo Zancan <ricardozancanc@gmail.com>
2024-09-17 22:27:53 -0300

    readme v1

--- tests/system/InterfaceTest.spec.cjs
+++ tests/system/InterfaceTest.spec.cjs
@@ -4,7 +4,6 @@ const { test, expect } = require('@playwright/test');
 test('has title', async ({ page }) => {
   await page.goto('http://127.0.0.1:8000');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Holiday Search/);
 });
 
